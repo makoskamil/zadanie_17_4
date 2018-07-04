3b231cb8bf4bf19a2f92c92514dea79653c7cf04
Kamil Makoś <makos.kamil@o2.pl>
2018-07-04 18:00:21 +0200

    Fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static('assets'));
 
-app.get('/:id', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile('/index.html');
 });
 
