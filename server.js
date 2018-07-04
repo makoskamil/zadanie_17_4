8a02d457df2c1c31f1c24ddb38c716d5d6035671
Kamil Makoś <makos.kamil@o2.pl>
2018-07-04 17:06:37 +0200

    change_files

--- server.js
+++ server.js
@@ -1,34 +1,22 @@
 var express = require('express');
-
 var app = express();
+app.use(express.static('assets'));
 
 app.get('/:id', function(req, res) {
-    console.log('Otrzymałem żądanie GET od strony głównej');
-    res.send('Identyfikator, który został dopisany to ' + req.params.id);
+    res.sendFile('/index.html');
 });
 
-app.post('/', function (req, res) {
-    console.log('Otrzymałem żądanie POST do strony głównej');
-    res.send('Hello Send');
-});
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
+})
 
-app.delete('/del_user', function(req, res) {
-    console.log('Otrzymałem żądanie DELETE do strony /del_user');
-    red.send('Hello DELETE');
-});
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-app.get('/list_user', function (req, res) {
-    console.log('Otrzymałem żądanie GET do strony /list_user');
-    res.send('Strona z listą użytkowników!');
-});
-
-app.get('/ab*cd', function(req, res) {
-    console.log('Otrzymałem żądanie GET do strony /ab*cd');
-    res.send('Wzór pasuje');
-});
-
-app.listen(3000);
-
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
